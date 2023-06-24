126dd39003d4fc1652250a28a9da67083e815221
jcoop32 <jcooper11@hawk.iit.edu>
2023-06-24 01:16:46 -0500

    show page for each item

--- controller/designs.js
+++ controller/designs.js
@@ -1,12 +1,26 @@
-const Work = require('../models/design');
+const Design = require('../models/design');
 module.exports = {
   index,
+  show,
 };
 
 async function index(req, res) {
   try {
+    const alldesigns = await Design.getAll();
     res.render('designs/index', {
-      work: await Work.getAll(),
+      work: alldesigns,
+      title: 'Designs',
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function show(req, res) {
+  try {
+    const oneDesign = await Design.getOne(req.params.id);
+    res.render('designs/show', {
+      work: oneDesign,
       title: 'Designs',
     });
   } catch (err) {
--- routes/designs.js
+++ routes/designs.js
@@ -6,4 +6,6 @@ const worksCtrl = require('../controller/designs');
 /* GET users listing. */
 router.get('/', worksCtrl.index);
 
+router.get('/:id', worksCtrl.show);
+
 module.exports = router;
